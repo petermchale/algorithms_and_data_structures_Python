a4ca97c4e04f104202e955d0f19d297e529b7ea6
Peter McHale <peter.thomas.mchale@gmail.com>
2018-01-03 16:37:32 -0800

    xxx

---
+++ binary search tree/binary_search_tree.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value <= self.data:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, value):
+        if value == self.data:
+            return True
+        elif value < self.data:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(value)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(value)
+
+
+
+
