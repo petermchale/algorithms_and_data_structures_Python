670273d03c90ab25f8d63bf0fff9d38e1c900804
Peter McHale <peter.thomas.mchale@gmail.com>
2022-03-29 21:08:22 -0600

    invert binary search tree

--- binary search tree/binary_search_tree.py
+++ binary search tree/binary_search_tree.py
@@ -33,7 +33,7 @@ class Node:
     def print_in_order(self):
         if self.left is not None:
             self.left.print_in_order()
-        print self.data
+        print(self.data)
         if self.right is not None:
             self.right.print_in_order()
 
@@ -47,26 +47,26 @@ def build_tree(l=[4, 2, 5, 1, 3, 6]):
 
 def traverse_tree():
     tree = build_tree()
-    print 'traversing tree in order:'
+    print('traversing tree in order:')
     tree.print_in_order()
-    print ''
+    print('')
 
 
 def search_tree():
     tree = build_tree()
-    print 'searching tree:'
-    print 'tree contains 4:', tree.contains(4)
-    print 'tree contains 10:', tree.contains(10)
+    print('searching tree:')
+    print('tree contains 4:', tree.contains(4))
+    print('tree contains 10:', tree.contains(10))
 
 
 def check_BST():
-    print ''
-    print 'create BST and check:'
+    print('')
+    print('create BST and check:')
     root = build_tree()
-    print 'tree is BST:', check_BST_core(root)
-    print 'remove the BST property and check:'
+    print('tree is BST:', check_BST_core(root))
+    print('remove the BST property and check:')
     root.left.right.data = 7
-    print 'tree is BST:', check_BST_core(root)
+    print('tree is BST:', check_BST_core(root))
 
 
 def check_BST_core(node, node_min=-100, node_max=100):
@@ -76,9 +76,34 @@ def check_BST_core(node, node_min=-100, node_max=100):
         return False
     return check_BST_core(node.left, node_min, node.data-1) and check_BST_core(node.right, node.data+1, node_max)
 
+def invert_tree():
+   tree = build_tree()
+   print('')
+   print('tree before inversion:')
+   tree.print_in_order()
+
+   print('') 
+   print('inverting tree...') 
+   inverted_tree = invert_tree_core(tree)
+
+   print('') 
+   print('tree after inversion:')
+   tree.print_in_order()
+
+def get_data(node): 
+   return node.data if node else None 
+
+def invert_tree_core(root):
+   # https://favtutor.com/blogs/invert-binary-tree
+   if root == None: return None
+   print(f'parent: {root.data}') 
+   root.left, root.right = invert_tree_core(root.right), invert_tree_core(root.left)
+   print(f'children: {get_data(root.left)} {get_data(root.right)}')
+   return root
 
 if __name__ == '__main__':
     traverse_tree()
     search_tree()
     check_BST()
+    invert_tree()
 
