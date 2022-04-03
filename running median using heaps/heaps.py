7b4e3cf47c8e583b4dae51de2e3f9f51c290a9b3
Peter McHale <peter.thomas.mchale@gmail.com>
2022-04-03 08:20:33 -0600

    print max heap

--- running median using heaps/heaps.py
+++ running median using heaps/heaps.py
@@ -34,7 +34,7 @@ class Max_Heap(Heap):
         return -heappop(self.container)
 
     def __repr__(self):
-        return str(map(lambda x: -x, self.container))
+        return str([-x for x in self.container])
 
     def top(self):
         return -self.container[0]
@@ -45,14 +45,14 @@ def test_min_heap(data=[1, 3, 5, 7, 9, 2, 4, 6, 8, 0]):
     min_heap = Min_Heap()
     for item in data:
         min_heap.push(item)
-    print 'min heap container: '
-    print min_heap
+    print('min heap container: ')
+    print(min_heap)
 
     ordered = []
     while min_heap.is_not_empty():
         ordered.append(min_heap.pop())
-    print 'popping elements until empty:'
-    print ordered
+    print('popping elements until empty:')
+    print(ordered)
 
 
 def test_max_heap(data=[1, 3, 5, 7, 9, 2, 4, 6, 8, 0]):
@@ -60,14 +60,14 @@ def test_max_heap(data=[1, 3, 5, 7, 9, 2, 4, 6, 8, 0]):
     max_heap = Max_Heap()
     for item in data:
         max_heap.push(item)
-    print 'max heap container: '
-    print max_heap
+    print('max heap container: ')
+    print(max_heap)
 
     ordered = []
     while max_heap.is_not_empty():
         ordered.append(max_heap.pop())
-    print 'popping elements until empty:'
-    print ordered
+    print('popping elements until empty:')
+    print(ordered)
 
 
 if __name__ == '__main__':
